fa80375200982f24dacf17dd3836e16fb31a3c45
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-06-21 22:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
+const container = document.getElementById("output");
+
 let prom = new Promise((resolve) => {
 	setTimeout(resolve, 1000);
 });
 
-prom.then( () => {console.log("Hello, world!")});
+prom.then( () => {
+	container.innerHTML = "Hello, world!";
+});
